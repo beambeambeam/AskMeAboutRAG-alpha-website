530aa65159ee92af0cea7ccaea0b0710e88eff99
beambeambeam <supawitmarayat@gmail.com>
2024-11-03 16:00:14 +0700

    feat: provider button

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header"
 export default function Home() {
   return (
     <div className="grid w-full grid-cols-5 gap-5 h-screen">
-      <main className="col-span-2 col-end-4 px-5 mt-4">
+      <main className="col-span-3 col-start-2 px-5 mt-4">
         <Header />
       </main>
     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,14 @@ import ModeToggle from "./mode-toggle"
 const Header = () => {
   return (
     <header className="flex flex-col gap-5 text-foreground">
-      <div className="flex flex-row items-center gap-5">
-        <Profile type="BOT" className="size-10" />
-        <h1 className="font-bold text-xl">AskMeAboutRAG</h1>
-        <ModeToggle />
+      <div className="w-full justify-between flex-row flex">
+        <div className="flex flex-row items-center gap-5">
+          <Profile type="BOT" className="size-10" />
+          <h1 className="font-bold text-xl">AskMeAboutRAG</h1>
+        </div>
+        <div className="flex flex-row gap-5">
+          <ModeToggle />
+        </div>
       </div>
       <p className="text-base font-semibold">
         How much do you know about your-self? neither LLMs do?
