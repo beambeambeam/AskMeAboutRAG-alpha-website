1a7a45cf60fd2e1a3e6c0bdc08a242c16b075fb0
beambeambeam <supawitmarayat@gmail.com>
2024-11-03 15:12:24 +0700

    feat: cookie toaster

--- app/wrapper.tsx
+++ app/wrapper.tsx
@@ -2,16 +2,29 @@
 
 import { useCookies } from "react-cookie"
 
+import { useToast } from "@/hooks/use-toast"
+import { Toaster } from "@/components/ui/toaster"
+
 export default function Wrapper({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>): JSX.Element {
   const [cookies, setCookie] = useCookies(["uuid"])
+  const { toast } = useToast()
 
   if (!cookies.uuid) {
     setCookie("uuid", Math.random().toString(36).slice(2))
+    toast({
+      title: "Welcome! ✨",
+      description: "This site uses cookies 🍪 to enhance your experience.",
+    })
   }
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      <Toaster />
+    </>
+  )
 }
