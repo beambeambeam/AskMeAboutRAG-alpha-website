a997434b2bfc19182a18b768abc0d76c4ae2002e
beambeambeam <supawitmarayat@gmail.com>
2024-11-05 09:49:26 +0700

    feat: animated button sent

--- components/chat.tsx
+++ components/chat.tsx
@@ -2,14 +2,17 @@ import React from "react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { ArrowRightIcon } from "@/components/animated/arrow-right"
 
 const Chat = () => {
   return (
     <div className="h-full w-full grid grid-rows-[1fr_auto]">
       <div>chat</div>
       <div className="flex w-full items-center space-x-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Subscribe</Button>
+        <Input type="text" placeholder="Ask me!" />
+        <Button type="submit" size="icon">
+          <ArrowRightIcon />
+        </Button>
       </div>
     </div>
   )
